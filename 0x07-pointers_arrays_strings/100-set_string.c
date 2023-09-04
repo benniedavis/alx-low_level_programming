25c154cf9ed8f3500cf5b5cd6208c9b4d8e63dcc
bennied865@gmail.com <bennied865@gmail.com>
2023-09-04 05:10:00 -0700

    Even More pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a poiner o a char
+ * @s: pointer to poiner
+ * @to: pointer char
+ */
+
+void set_string(char **s,char *to)
+{
+	*s = to;
+}
